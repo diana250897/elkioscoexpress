3968871f3373b746697b562dbd290badfe386647
diana970825 <dfpuentesv@correo.udistrital.edu.co>
2021-04-23 14:22:55 -0500

    prueba de otra ruta

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/diana', function(req, res, next) {
+  res.render('diana', { title: 'Express' });
+});
 module.exports = router;
